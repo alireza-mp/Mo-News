1a94b6b63f02c2540f5695fa4bca252e72aa696c
Alireza Momenpour <70968053+alireza-mp@users.noreply.github.com>
2023-06-18 21:22:04 +0330

    change app language && change systemBar and statusBar color

--- app/src/main/java/ir/alirezamp/monews/MainActivity.kt
+++ app/src/main/java/ir/alirezamp/monews/MainActivity.kt
@@ -1,34 +1,45 @@
 package ir.alirezamp.monews
 
+import android.app.Activity
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import ir.alirezamp.components.BottomNavigation
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import ir.alirezamp.designsystem.base.BaseViewModel
 import ir.alirezamp.designsystem.theme.MoNewsTheme
 import ir.alirezamp.monews.navigation.ComposeNewsNavHost
+import ir.alirezamp.navigation.BottomNavigation
+import java.util.Locale
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setAppLanguage(this)
         setContent {
-
             var baseViewModel: BaseViewModel by remember {
                 mutableStateOf(BaseViewModel())
             }
@@ -38,7 +49,7 @@ class MainActivity : ComponentActivity() {
             /*val bottomNavVisible = */
 
             MoNewsTheme {
-
+                SetStatusBarColor()
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     contentColor = MaterialTheme.colorScheme.background,
@@ -87,4 +98,40 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Composable
+private fun SetStatusBarColor() {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = !isSystemInDarkTheme()
+    val systemBarColor = MaterialTheme.colorScheme.surfaceVariant
+    val statusBarColor = MaterialTheme.colorScheme.surface
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = statusBarColor.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = useDarkIcons
+        }
+    }
+
+    DisposableEffect(systemUiController, useDarkIcons) {
+        systemUiController.setSystemBarsColor(
+            color = systemBarColor,
+            darkIcons = useDarkIcons
+        )
+        systemUiController.setStatusBarColor(
+            color = statusBarColor,
+            darkIcons = useDarkIcons
+        )
+        onDispose {}
+    }
+}
+
+private fun setAppLanguage(activity: Activity, language: String = "fa") {
+    val locale = Locale(language)
+    Locale.setDefault(locale)
+    val config: Configuration = activity.resources.configuration
+    config.setLocale(locale)
+    activity.resources.updateConfiguration(config, activity.resources.displayMetrics)
 }
